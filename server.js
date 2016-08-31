3438a858235fdb66648dc4dc16f185300b6371a8
paigen11 <paigen11@gmail.com>
2016-08-31 11:56:37 -0400

    Updates - still buggy

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ var fs = require('fs');
 //server is what happens when someone loads up the page in a browser. 
 //server is listening below for http traffic at port XXXX
 var server = http.createServer(function(req, res){
+	// if you were using a separate css file
+	// if(req.url === '/styles.css'){
+		// fs.readFile('index.html', 'utf-8', function(error, data){
+		// 	// console.log(error);
+		// 	// console.log(data);
+		// 	if(error){
+		// 		res.writeHead(500, {'content-type': 'text/html'});
+		// 		res.end(error);
+		// 	}else{
+		// 		res.writeHead(200, {'content-type': 'text/html'});	
+		// 		res.end(data);	
+		// 	}
+		// });
+	// }
+
 	fs.readFile('index.html', 'utf-8', function(error, data){
 		// console.log(error);
 		// console.log(data);
@@ -24,15 +39,33 @@ var io = socketIo.listen(server);
 var socketUsers = [];
 // we need to deal with a new socket connection
 io.sockets.on('connect', function(socket){
-	socketUsers.push(socket);
 	
+	// console.log(socket);
+	socketUsers.push({
+		socketID: socket.id,
+		name: 'Anonymous'
+	});
+
+	//this object is 'data' on index.html's 'users' socket
+	io.sockets.emit('users', socketUsers); 
+	// {
+	// 	socketUsers: socketUsers	
+	// });
+	
+	// socketUsers.push(socket);
+	console.log("someone connected via a socket");
+	
+	//someone just changed their name
 	socket.on('name_to_server', function(name){
-		io.sockets.emit('users', {
-			name: name.name	
-		});
+		for(var i = 0; i < socketUsers.length; i++){
+			if(socketUsers[i].socketID == socket.id){
+				socketUsers[i].name = name;
+				break;
+			}
+		}
+		io.sockets.emit('users', socketUsers);	
 	});	
 
-	console.log("someone connected via a socket");
 	socket.on('message_to_server', function(data){
 		io.sockets.emit('message_to_client', {
 			message: data.message,
@@ -42,11 +75,15 @@ io.sockets.on('connect', function(socket){
 	});
 	socket.on('disconnect', function(){
 		console.log("A user has disconnected");
-		var user = socketUsers.indexOf(socket);
+		for(var i = 0; i < socketUsers.length; i++){
+			if(socketUsers[i].socketID == socket.id){
+				socketUsers.splice(i, 1);
+				break;
+			}
+		}
+		// var user = socketUsers.indexOf(socket);
 		// socketUsers.splice(user,1);
-		// io.sockets.emit('user_left', function(name){
-		// 	name: name.name
-		// });
+		
 	});
 });
 
